97371e8bc648a0d1368dbc99f680442d9b42a49f
steed <892437476@qq.com>
2021-12-29 09:55:02 +0800

    环境配置

--- static/config/index-qa.js
+++ static/config/index-qa.js
@@ -5,7 +5,7 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'https://test-victory-down.ruiztech.cn:4443/ruiz';
+  window.SITE_CONFIG['baseUrl'] = 'http://assets-manage-serverless.ruiztech.cn/ruiz';
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
